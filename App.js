736478521620e03830376068faddf28d29534343
Ove <osundal83@gmail.com>
2018-02-20 22:11:05 +0100

    feat: add tabnavigation

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {createStore, applyMiddleware, compose} from 'redux';
-import rootReducer from './src/reducers'
-import DeckList from "./src/components/views/DeckList";
+import {StyleSheet, View} from 'react-native';
+import Decks from "./src/components/view/Decks";
+import NewDeck from "./src/components/view/NewDeck";
+import {TabNavigator} from "react-navigation";
 import {Provider} from "react-redux";
+import {createStore} from "redux";
+import rootReducer from './src/reducers/index';
+
 
 export default class App extends React.Component {
   render() {
     return (
         <Provider store={createStore(rootReducer)}>
             <View style={styles.container}>
-            <DeckList/>
+                <Tabs />
             </View>
         </Provider>
     );
@@ -19,9 +22,16 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+});
+
+const Tabs = TabNavigator({
+    'Decks': {
+        screen: Decks,
+        // }
+    },
+    'Add Entry': {
+        screen: NewDeck,
+    }
 });
