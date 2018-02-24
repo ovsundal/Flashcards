652d9ecaca07fde25315575ecd6b42bd8531dd26
Ove <osundal83@gmail.com>
2018-02-24 18:25:19 +0100

    create component deck

---
+++ src/components/Deck.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+function Deck(props) {
+    return(
+        <View>
+            <Text>{props.title}</Text>
+        </View>
+    )
+}
