fb7075fc402286d3493c57fd92ae2f81eeba0076
Ove <osundal83@gmail.com>
2018-03-01 19:03:36 +0100

    create form for creating new question

--- src/components/view/NewQuestion.js
+++ src/components/view/NewQuestion.js
@@ -1,8 +1,21 @@
 import React from "react";
 import {Button, Text, TextInput, View} from "react-native";
+import t from 'tcomb-form-native';
+
+//used this guide for creating form: https://medium.com/react-native-development/easily-build-forms-in-react-native-9006fcd2a73b
+const Form = t.form.Form;
+
+const Question = t.struct({
+    question: t.String,
+    answer: t.String
+});
 
 export default class NewQuestion extends React.Component {
 
+    handleSubmit = () => {
+        const value = this._form.getValue();
+        console.log(value)
+    };
 
     render() {
         const deck = this.props.navigation.state.params;
@@ -10,7 +23,13 @@ export default class NewQuestion extends React.Component {
             <View>
                 <Text>Current deck: {deck.title}</Text>
 
-                <Text></Text>
+                <Form
+                    ref={c => this._form = c}
+                    type={Question} />
+                <Button
+                title='Submit'
+                onPress={this.handleSubmit}
+                />
 
             </View>
         )
